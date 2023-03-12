754280a9b62b69a68c20f9b7f6c9596d59844ff6
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-11 19:25:58 -0500

    fixup! aml: Categorize permissions
    Change-Id: Ife3120622b6216a6da1bce2efc17e15e839be160

--- amlogic.mk
+++ amlogic.mk
@@ -172,7 +172,7 @@ endif
 PRODUCT_COPY_FILES +=  \
     device/amlogic/common/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-PRODUCT_PACKAGES += \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
