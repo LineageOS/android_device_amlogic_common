abd3cace093084ad9cad9629a38d0040dfc299d0
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-08 17:41:33 -0400

    aml: Decommonize NRDP
    Change-Id: I541d7fd52e929ea3af3fe25e00f9d0fa65ee6797

--- amlogic.mk
+++ amlogic.mk
@@ -130,13 +130,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service
 endif
 
-## NRDP (Netflix)
-PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/nrdp/nrdp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/nrdp.xml \
-    $(LOCAL_PATH)/nrdp/nrdp_audio_platform_capabilities.json:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/nrdp_audio_platform_capabilities.json \
-    $(LOCAL_PATH)/nrdp/nrdp_audio_platform_capabilities_ms12.json:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/nrdp_audio_platform_capabilities_ms12.json \
-    $(LOCAL_PATH)/nrdp/nrdp_platform_capabilities.json:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/nrdp_platform_capabilities.json
-
 ## Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
