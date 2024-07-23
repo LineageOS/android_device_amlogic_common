e9e8bbcb3b9770a7a5a4979200ef18d35df98d11
Bruno Martins <bgcngm@gmail.com>
2024-07-23 11:15:20 +0000

    Revert "aml: Do not set product characteristics for Automotive"
    This reverts commit bc5fc71bc5d637a4ff144d698271787f2d16ab77.
    
    Reason for revert: logic around PRODUCT_IS_AUTOMOTIVE was only tested
    in lineage-21 branch.
    
    Change-Id: If0609811dd1ea0378b2b5f1a9271708d89353f82

--- amlogic.mk
+++ amlogic.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2024 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -62,8 +62,8 @@ endif
 ## Characteristics
 ifeq ($(PRODUCT_IS_ATV),true)
 PRODUCT_CHARACTERISTICS := tv
-else ifneq ($(PRODUCT_IS_AUTOMOTIVE),true)
-PRODUCT_CHARACTERISTICS := tablet
+else
+PRODUCT_CHARACTERISTICS ?= tablet
 endif
 
 ## Codecs
