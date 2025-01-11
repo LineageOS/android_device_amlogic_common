d31be6076d6cd558009064b3b4eaac0286aa7913
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-11 02:17:11 -0500

    aml: Convert logo image generation to build/tasks
    Change-Id: I49e9b67c3d667d4b24dd6a2612190f143381d0a6

--- build/tasks/logo.mk
+++ build/tasks/logo.mk
@@ -1,10 +1,10 @@
 #
-# Copyright (C) 2022-2024 The LineageOS Project
+# Copyright (C) 2022-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-include $(CLEAR_VARS)
+ifneq ($(filter gxl gxm g12a g12b sm1 s4 t7 sc2, $(TARGET_AMLOGIC_SOC)),)
 
 COMMON_LOGO_FILES := device/amlogic/common/logo
 
@@ -24,10 +24,13 @@ $(INSTALLED_LOGOIMAGE_TARGET): $(LOCAL_INSTALLED_MODULE) | $(IMGPACK) $(MINIGZIP
 	$(hide) $(IMGPACK) -r $(PRODUCT_OUT)/logo $(INSTALLED_LOGOIMAGE_TARGET)
 	$(hide) rm -rf $(PRODUCT_OUT)/logo
 
-ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_LOGOIMAGE_TARGET)
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_LOGOIMAGE_TARGET)
+.PHONY: logoimage
+logoimage: $(INSTALLED_LOGOIMAGE_TARGET)
+
+droidcore: logoimage
+
+$(call dist-for-goals, dist_files, $(INSTALLED_LOGOIMAGE_TARGET))
 
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_LOGOIMAGE_TARGET)
 
-.PHONY: logoimage
-logoimage: $(INSTALLED_LOGOIMAGE_TARGET)
+endif
