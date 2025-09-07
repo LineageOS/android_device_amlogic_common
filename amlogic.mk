372fc0b9eeb49078b6a259388a6e7cf584f711e2
Nolen Johnson <johnsonnolen@gmail.com>
2025-09-07 02:26:26 -0400

    aml: Allow devices to override the logo
    Change-Id: Ib4b32b6085d7228d7d415d9e82c58d72777dc226

--- amlogic.mk
+++ amlogic.mk
@@ -113,9 +113,9 @@ endif
 
 ## Logo
 ifneq ($(PRODUCT_IS_ATV),true)
-LOGO_FILES := $(LOCAL_PATH)/logo/mobile
+LOGO_FILES ?= $(LOCAL_PATH)/logo/mobile
 else
-LOGO_FILES := $(LOCAL_PATH)/logo/tv
+LOGO_FILES ?= $(LOCAL_PATH)/logo/tv
 endif
 
 PRODUCT_HOST_PACKAGES += \
