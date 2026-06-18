3a08a8e9ef6df736159fa51ec32aaf385ec9c8f9
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-06-18 10:41:27 +0800

    aml: Exclude some services for 5.15
    * 5.15 uses droidlogic variants of these.
    
    Change-Id: I46b86cb31cdb64a7c2c7c4ca865dc0baea72581d

--- amlogic.mk
+++ amlogic.mk
@@ -22,10 +22,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.output.xml
 
+ifeq ($(TARGET_KERNEL_VERSION),5.4)
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service
+endif
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.service \
     audio.r_submix.default \
     audio.usb.default
 
@@ -74,9 +78,11 @@ PRODUCT_PACKAGES += \
     libz_stable.vendor
 
 ## fastbootd
+ifeq ($(TARGET_KERNEL_VERSION),5.4)
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl.custom \
     fastbootd
+endif
 
 ## Gatekeeper
 PRODUCT_PACKAGES += \
@@ -93,9 +99,11 @@ PRODUCT_PACKAGES += \
 
 ## HDMI CEC
 ifeq ($(PRODUCT_IS_ATV),true)
+ifeq ($(TARGET_KERNEL_VERSION),5.4)
 PRODUCT_PACKAGES += \
     android.hardware.tv.cec@1.0-impl \
     android.hardware.tv.cec@1.0-service
+endif
 
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hdmi.cec.xml
@@ -165,8 +173,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 ## USB
+ifeq ($(TARGET_KERNEL_VERSION),5.4)
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
+endif
 
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
