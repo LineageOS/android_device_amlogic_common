3187a723a42383d4e8dfd338db9392e2d874033f
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-03 13:19:57 -0400

    aml: Drop 5.4 kernel support
    Change-Id: I6d3298de511e9916bbf843c91b941a1390347ee6

--- amlogic.mk
+++ amlogic.mk
@@ -22,11 +22,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.output.xml
 
-ifeq ($(TARGET_KERNEL_VERSION),5.4)
-PRODUCT_PACKAGES += \
-    android.hardware.audio.service
-endif
-
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
@@ -97,12 +92,6 @@ PRODUCT_PACKAGES += \
 
 ## HDMI CEC
 ifeq ($(PRODUCT_IS_ATV),true)
-ifeq ($(TARGET_KERNEL_VERSION),5.4)
-PRODUCT_PACKAGES += \
-    android.hardware.tv.cec@1.0-impl \
-    android.hardware.tv.cec@1.0-service
-endif
-
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hdmi.cec.xml
 endif
@@ -171,11 +160,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 ## USB
-ifeq ($(TARGET_KERNEL_VERSION),5.4)
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-endif
-
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
