083dbc538985e70de1c452ba94790868224fd34c
Bruno Martins <bgcngm@gmail.com>
2026-08-30 11:07:57 +0100

    aml: build/tasks: Package the DTB as a radio image on pre-ne SoC's
    Without this build task recipe the generated dtb.img is not present
    in the OTA .zip, after making the switch to platform-kernel.
    
    Nuke TARGET_FLASH_DTB_PARTITION with it, since the SoC guard is
    more than sufficient.
    
    Change-Id: I6dc9abbdefe75acdbc3e217e2c9b53d7fc89c623

---
+++ build/tasks/dtb.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifneq ($(filter gxl gxm g12a g12b sm1, $(TARGET_AMLOGIC_SOC)),)
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_DTBIMAGE_TARGET)
+
+endif
